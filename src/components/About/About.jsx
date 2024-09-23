ddf92bcb5bbe5a7f357f9a1db1e814d77f872f06
Deinigu <diegolopezreduello@gmail.com>
2024-09-23 11:40:01 +0200

    ✨About section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>ABOUT</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="About me"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Prueba</h3>
+              <p>Esto es una prueba no tengan en cuenta</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Prueba 2</h3>
+              <p>Esto es otra prueba por favor no tengan en cuenta gracias</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/uiIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Prueba 3</h3>
+              <p>Soy buenísimo con las interfaces illo contratame.</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
