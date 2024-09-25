bc4a96d5f24988f37f9dc60973651dab944d8aa9
Deinigu <diegolopezreduello@gmail.com>
2024-09-25 15:55:52 +0200

    ✨Routing implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import styles from "./App.module.css";
 import { Navbar } from "./components/NavBar/Navbar";
-import { Hero } from "./components/Hero/Hero";
-import { About } from "./components/About/About";
-import { Experience } from "./components/Experience/Experience";
-import { Projects } from "./components/Projects/Projects";
+import { About } from "./pages/About/About";
+import { Experience } from "./pages/Experience/Experience";
+import { Projects } from "./pages/Projects/Projects";
 import { Footer } from "./components/Footer/Footer";
+import { Home } from "./pages/Home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
-      <Hero />
-      <About />
-      <Experience />
-      <Projects />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
       <Footer />
     </div>
   );
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Hero } from "../../components/Hero/Hero";
+import styles from "./Home.module.css";
+
+
+export const Home = () => {
+  return (
+      <Hero />
+  );
+};
