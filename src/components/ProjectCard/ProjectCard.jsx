49ca0b6cc7ad1348b238bed35f718666e8906909
Deinigu <diegolopezreduello@gmail.com>
2024-09-30 18:37:40 +0200

    ✨New project card design

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -13,6 +13,7 @@ export const ProjectCard = ({
         alt={`Image of  ${title}`}
         className={styles.image}
       />
+      <div className={styles.content}>
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
       <ul className={styles.skills}>
@@ -24,13 +25,6 @@ export const ProjectCard = ({
           );
         })}
       </ul>
-      <div className={styles.links}>
-        <a href={demo} className={styles.link}>
-          Demo
-        </a>
-        <a href={source} className={styles.link}>
-          Source
-        </a>
       </div>
     </div>
   );
